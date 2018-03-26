e1d777b527e4ec90603380d757e52dc78da3f144
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2018-03-26 17:38:48 +0600

    Form

--- example.go
+++ example.go
@@ -2,27 +2,62 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()       // разбор аргументов, необходимо вызвать самостоятельно
-	fmt.Println(r.Form) // печать данных формы на стороне сервера
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
+	r.ParseForm() // Анализирует переданные параметры url, затем анализирует пакет ответа для тела POST (тела запроса)
+	// внимание: без вызова метода ParseForm последующие данные не будут получены
+	fmt.Println(r.Form) // печатает информацию на сервере
+	fmt.Println("Путь: ", r.URL.Path)
+	fmt.Println("Схема: ", r.URL.Scheme)
 	fmt.Println(r.Form["url_long"])
 	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
+		fmt.Println("Ключ: ", k)
+		fmt.Println("Значение: ", strings.Join(v, ""))
 	}
-	fmt.Fprintf(w, "Hello Pavel!") // отправка данных на клиент
+	fmt.Fprintf(w, "Привет Pavel!") // пишет данные в ответ
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("Метод:", r.Method) // получаем информацию о методе запроса
+	if r.Method == "GET" {
+		t, _ := template.ParseFiles("login.html")
+		t.Execute(w, nil)
+	} else {
+
+		fmt.Println(r.FormValue("username")) // Go вызовет r.ParseForm автоматически, вернет первое из значений
+
+		r.ParseForm()
+		// логическая часть процесса входа
+		fmt.Println("Пользователь:", r.Form["username"])
+		fmt.Println("Пароль:", r.Form["password"])
+
+		v := url.Values{}
+
+		v.Set("name", "Ava")
+		v.Add("name", "Pavel")
+		v.Add("friend", "Jess")
+		v.Add("friend", "Sarah")
+		v.Add("friend", "Zoe")
+		// v.Encode() == "name=Ava&friend=Jess&friend=Sarah&friend=Zoe"
+		fmt.Println(v.Get("name"))   // безопаснее использовать Get т.к. возращает пустое значение если пусто
+		fmt.Println(v.Get("friend")) // только одно первое значение
+		fmt.Println(v["friend"])     // карта значений
+		fmt.Println(v["name"])       // карта значений
+
+	}
 }
 
 func main() {
-	http.HandleFunc("/", sayhelloName)       // устанавливаем обработчик
-	err := http.ListenAndServe(":9090", nil) // устанавливаем порт, который будем слушать
+	http.HandleFunc("/", sayhelloName) // устанавливаем правила маршрутизатора
+	http.HandleFunc("/login", login)
+	err := http.ListenAndServe(":9090", nil) // устанавливаем порт для прослушивания
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
