5a8753b1f3682851324788641f28d1fc72b0c317
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2018-03-28 22:38:53 +0600

    Func upload

--- example.go
+++ example.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -54,9 +55,41 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// обработка закачки
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Метод:", r.Method)
+	fmt.Println("upload")
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("upload.html")
+		t.Execute(w, token)
+	} else {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile("/test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", sayhelloName) // устанавливаем правила маршрутизатора
 	http.HandleFunc("/login", login)
+	http.HandleFunc("/upload", upload)
 	err := http.ListenAndServe(":9090", nil) // устанавливаем порт для прослушивания
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
